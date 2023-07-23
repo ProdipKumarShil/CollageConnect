1b20cd7100fe8ba75eddd99fdbb03f2a033c5d93
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-07-23 13:45:28 +0600

    added sign in

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,15 +1,36 @@
 import Lottie from "lottie-react";
 import animation from '../../assets/lottie/Login.json'
+import { AuthContext } from "../../provider/AuthProvider";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
+  const { googlePopUpSignin, setUser, emailSignIn } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const handleSignin = e => {
+    e.preventDefault()
+    const form = e.target
+    const email = form.email.value
+    const password = form.password.value
+    
+    emailSignIn(email, password)
+      .then(result => {
+        setUser(result.user)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 items-center mb-4">
       <div className="">
         <h1 className="text-5xl font-bold text-center mb-8">Sing In Here</h1>
-        <form className="flex flex-col items-center justify-center gap-4 px-8" >
+        <form onSubmit={handleSignin} className="flex flex-col items-center justify-center gap-4 px-8" >
           <input name="email" type="email" placeholder="Email" className="input input-bordered input-info w-full " />
           <input name="password" type="password" placeholder="Password" className="input input-bordered input-info w-full " />
-          <button className="btn btn-info w-full">Sign up</button>
+          <button className="btn btn-info w-full">Sign in</button>
         </form>
 
         {/* social services */}
