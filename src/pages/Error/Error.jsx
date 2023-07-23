38a9566fe4ce95b75e154c981c6d16cf812f6ecb
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-07-23 18:11:46 +0600

    added error pages

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,11 @@
-
+import Lottie from "lottie-react";
+import error from '../../assets/lottie/error.json'
 
 const Error = () => {
   return (
-    <div>
-      error
+    <div className="h-100vh ">
+      <h1 className="text-5xl text-center mt-4">Page Not Found!</h1>
+      <Lottie className="w-10/12 h-[90vh] mx-auto" animationData={error} />
     </div>
   );
 };
